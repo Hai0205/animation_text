8d0b61f5087697a560d0ef6827674108efa3cc5b
hai <hai.alphacreative@gmail.com>
2024-10-07 11:01:40 +0700

    ad

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,6 +4,7 @@ $(document).ready(function () {
   effect2();
   effect3();
   effect4();
+  effect5();
 });
 function effect1() {
   const splitText = new SplitType(".effect-text ", { types: "chars" });
@@ -65,4 +66,53 @@ function effect4() {
     repeat: -1, // Infinite loop
     repeatDelay: 0,
   });
+}
+function effect5() {
+  var tmax_optionsGlobal = {
+    repeat: -1,
+    repeatDelay: 0.65,
+    yoyo: true,
+  };
+
+  CSSPlugin.useSVGTransformAttr = true;
+
+  var tl = new TimelineMax(tmax_optionsGlobal),
+    path = "svg *",
+    stagger_val = 0.0125,
+    duration = 0.75;
+
+  $.each($(path), function (i, el) {
+    tl.set($(this), {
+      x: "+=" + getRandom(-500, 500),
+      y: "+=" + getRandom(-500, 500),
+      rotation: "+=" + getRandom(-720, 720),
+      scale: 0,
+      opacity: 0,
+    });
+  });
+
+  var stagger_opts_to = {
+    x: 0,
+    y: 0,
+    opacity: 1,
+    scale: 1,
+    rotation: 0,
+    ease: Power4.easeInOut,
+  };
+
+  tl.staggerTo(path, duration, stagger_opts_to, stagger_val);
+
+  var $svg = $(".effect-5 svg");
+  $svg.hover(
+    function () {
+      tl.timeScale(0.15);
+    },
+    function () {
+      tl.timeScale(1);
+    }
+  );
+
+  function getRandom(min, max) {
+    return Math.random() * (max - min) + min;
+  }
 }
